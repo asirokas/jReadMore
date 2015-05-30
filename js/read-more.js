5015f7ab73425e33edcf03bfecf26c7749b331d3
Athanasios Sirokas <asirokas@gmail.com>
2015-05-30 19:52:44 +0300

    Converted script to jQurey plugin

--- js/read-more.js
+++ js/read-more.js
@@ -1,47 +1,61 @@
-// 0. Options
-var options = {
-	readMoreText: "Read more",
-	readLessText: "Read less",
-	readMoreHeight: 75
-};
+(function($) {
 
-$( ".read-more" ).each(function() {
-	// 1. Get the options for the specific element
-	// TODO: If there aren't any options set, use the defaults
-	options.readMoreHeight = $( this ).data( "options" );
-	console.log( options.readMoreHeight );
+	$.fn.readMore = function(options) {
 
-	// 2. Create the read-more link
-	$( this ).after( '<span class="read-more-link">' + options.readMoreText +'</span>' );
+		// 0. Options
+		var defaults = {
+			readMoreText: "Read more",
+			readLessText: "Read less",
+			readMoreHeight: 75
+		};
 
-	// 3. Set the initial state of the read more element to be collapsed
-	$( this ).css({
-		"height": options.readMoreHeight,
-		"overflow": "hidden"
+		// Merge deafults into options
+		var options = $.extend(defaults, options);
+
+		obj = $( this );
+
+		function addReadMoreElement(element) {
+			// 1. Get the options for the specific element
+			// TODO: If there aren't any options set, use the defaults
+			options.readMoreHeight = obj.data( "options" );
+			console.log( options.readMoreHeight );
+
+			// 2. Create the read-more link
+			obj.after( '<span class="read-more-link">' + options.readMoreText +'</span>' );
+
+			// 3. Set the initial state of the read more element to be collapsed
+			obj.css({
+				"height": options.readMoreHeight,
+				"overflow": "hidden"
+				});
+		}
+
+		addReadMoreElement(obj);
+
+		// 4. Action on clicking the read-more link
+		$( ".read-more-link" ).click(function() {
+			console.log( $( this ).text() );
+		    // Expand or collapse the "more" text
+			if ( $( this ).prev().css( "overflow" ) == "hidden" ) {
+				$( this ).prev().css({
+					"height": "auto",
+					"overflow": "auto"
+					});
+			} else {
+				$( this ).prev().css({
+					"height": options.readMoreHeight,
+					"overflow": "hidden"
+					});
+			};
+
+		    // Change the "read more" word accordingly
+		    if ( $( this ).text() == options.readMoreText ) {
+				$( this ).text( options.readLessText );
+		    } else {
+				$( this ).text( options.readMoreText );
+		    };
+
 		});
-});
-
-// 4. Action on clicking the read-more link
-$( ".read-more-link" ).click(function() {
-	console.log( $( this ).text() );
-    // Expand or collapse the "more" text
-	if ($( this ).prev().css( "overflow" ) == "hidden" ) {
-		$( this ).prev().css({
-			"height": "auto",
-			"overflow": "auto"
-			});
-	} else {
-		$( this ).prev().css({
-			"height": options.readMoreHeight,
-			"overflow": "hidden"
-			});
 	};
 
-    // Change the "read more" word accordingly
-    if ( $( this ).text() == options.readMoreText ) {
-        $( this ).text( options.readLessText );
-    } else {
-        $( this ).text( options.readMoreText );
-    };
-
-});
+})(jQuery);
