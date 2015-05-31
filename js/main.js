722fb1b0e215c771c99555c1516a8abf4922363c
Athanasios Sirokas <asirokas@gmail.com>
2015-05-31 15:57:25 +0300

    Changed the structure of the pproject's folder

--- js/main.js
+++
@@ -1,6 +0,0 @@
-// Code to load a script, only if a specific class exists.
-var element = $('.read-more');
-
-if( element.length > 0 ) {
-    $('body').append('<script src="js/read-more.js"></script>');
-}
